20e7779cc0c3f9097bdb012a456b55fc2ea8b214
Luis Díaz <ldiazn98@gmail.com>
2025-07-17 10:36:17 +0200

    Write response back to sender

--- ooniprobe-helpers/src/bin/jsonth.rs
+++ ooniprobe-helpers/src/bin/jsonth.rs
@@ -1,5 +1,5 @@
 use tokio::net::TcpStream;
-use tokio::io::{AsyncBufReadExt, BufReader};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter};
 use ooniprobe_helpers::helper_runner::run;
 use log::{error, info, debug};
 use serde::Serialize;
@@ -41,21 +41,24 @@ impl Response {
     }
 */
 async fn handle_json_helper(socket: TcpStream) {
-    let mut reader = BufReader::new(socket);
-    let mut buff = String::new();
+    let mut socket = socket;
+    let reader = BufReader::new(&mut socket);
     let mut resp = Response::new();
+
 
     // Read request line
     let mut lines = reader.lines();
     match lines.next_line().await {
         Ok(None) => {
-            error!("Connection closed by client")
+            error!("Connection closed by client");
+            return;
         }
         Ok(Some(l)) => {
             resp.request_line = l.trim().to_string();
         }
         Err(e) => {
-            error!("Error reading request: {}", e)
+            error!("Error reading request: {}", e);
+            return;
         }
     }
 
@@ -78,10 +81,15 @@ async fn handle_json_helper(socket: TcpStream) {
                         );
                 }
             }
-            Ok(None) => error!("Connection closed by client"),
-            Err(e) => error!("Could not read headers: {}", e)
+            Ok(None) => {
+                error!("Connection closed by client");
+                return;
+            },
+            Err(e) => panic!("Could not read headers: {}", e)
         }
     }
+    
+    log_response(&resp);
 
     // Write response back 
     let body = match serde_json::to_string(&resp){
@@ -91,4 +99,33 @@ async fn handle_json_helper(socket: TcpStream) {
         }
     };
 
+    let response = format!(
+                "HTTP/1.1 200 OK\r\nContent-Length: {}\r\nContent-Type:application/json\r\n\r\n{}",
+                body.len(),
+                body
+            );
+    
+    match socket.write_all(response.as_bytes()).await {
+        Ok(_) => debug!("Response sent successfully"),
+        Err(e) => error!("Couldn't write response back: {}", e)
+    }
+}
+
+fn log_response(resp: &Response) {
+    // request line, user agent, host
+    let mut user_agent = "<not provided>";
+    for header in &resp.request_headers {
+        if header[0].to_lowercase() == "user-agent" {
+            user_agent = header[1].as_str();
+        }
+    }
+
+    let mut host = "<not provided>";
+    for header in &resp.request_headers {
+        if header[0].to_lowercase() == "host" {
+            host = header[1].as_str();
+        }
+    }
+
+    info!("{} - User-Agent: {} - Host: {}", resp.request_line, user_agent, host);
 }
