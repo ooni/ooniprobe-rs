94ec44d4935b8fa78ba3d177239d97d740f84941
decfox <mehul@openobservatory.org>
2026-02-16 19:15:02 +0530

    feat: replace kotlin dir with android for aar builds

---
+++ android/ooniprobe/build.gradle.kts
@@ -0,0 +1,28 @@
+dependencies {
+    implementation "net.java.dev.jna:jna:5.14.0@aar"
+    implementation "org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version"
+}
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
+
+android {
+    namespace = "org.ooni.ooniprobe"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    sourceSets {
+        getByName("main") {
+            jniLibs.srcDirs("src/main/jniLibs")
+        }
+    }
+}
+
+dependencies {
+    implementation("net.java.dev.jna:jna:5.14.0@aar")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
+}
