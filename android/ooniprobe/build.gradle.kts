86838d6f7c52f5a999b5ec5e3a8217c79b0b2de2
decfox <mehul@openobservatory.org>
2026-02-16 19:55:48 +0530

    fix: use correct kts syntax for gradle

--- android/ooniprobe/build.gradle.kts
+++ android/ooniprobe/build.gradle.kts
@@ -1,10 +1,6 @@
-dependencies {
-    implementation "net.java.dev.jna:jna:5.14.0@aar"
-    implementation "org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version"
-}
 plugins {
     id("com.android.library")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
