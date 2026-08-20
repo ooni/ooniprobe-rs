92c47b09a9c2c3a828fa05b5a7e67eb90ee00b39
decfox <mehul@openobservatory.org>
2026-08-20 19:18:06 +0530

    feat: expose userauth protocol version as part of udl

--- ooniprobe-ffi/src/lib.rs
+++ ooniprobe-ffi/src/lib.rs
@@ -6,8 +6,8 @@ pub mod userauth;
 pub use client::{client_get, client_post, HttpResponse, KeyValue};
 pub use errors::OoniError;
 pub use userauth::{
-    get_probe_id, userauth_register, userauth_submit, CredentialConfig, CredentialResult,
-    ParamRange, ProbeIDResult,
+    get_probe_id, protocol_version, userauth_register, userauth_submit, CredentialConfig,
+    CredentialResult, ParamRange, ProbeIDResult,
 };
 
 // Required for UniFFI scaffolding
